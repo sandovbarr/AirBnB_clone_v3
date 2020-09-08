422d49484190122eafa4a26638a4a9762aa89cce
fernando <sandovbarr@gmail.com>
2020-09-07 21:40:50 -0500

    [MOVE] api folders from models to root

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" Flask App runner
-"""
-
+""" Flask App runner """
 from flask import Flask, Blueprint
 from models import storage
-from api.v1.views import app_views
+# from api.v1.views import app_views
+from os import getenv
+from api.v1.views.index import *
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
-HBNB_API_PORT = 5000
-HBNB_API_HOST = 0.0.0.0
+HBNB_API_PORT = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000 
+HBNB_API_HOST = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else "0.0.0.0"
 
 @app.teardown_appcontext
 def teardown_app(error):
--- models/api/v1/views/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-from flask import Blueprint
-from api.v1.views.index import *
-app_views = Blueprint('app_views',url_prefix='/api/v1')
