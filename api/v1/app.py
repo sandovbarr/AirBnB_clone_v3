ae8ae24d695bdf6e47db594b2a5fed6e7a4efd53
fernando <sandovbarr@gmail.com>
2020-09-10 18:10:46 -0500

    [FIX] import

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
+from api.v1.views.places_reviews import *
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
